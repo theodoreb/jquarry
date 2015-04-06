35906d5ac6aaf33000ec53feab82bb2de8e17fab
Théodore Biadala <theodore@biadala.net>
2015-04-06 22:59:48 +0200

    Dirty support for folders

--- bin/jquarry.js
+++ bin/jquarry.js
@@ -5,49 +5,55 @@ var program = require('commander');
 var scanFile = require('../lib/target/scan-jquery-use');
 var debug = require('debug')('main');
 var _ = require('underscore');
-
-program
-  .version('0.0.1');
-
-program
-  .command('scan <file>')
-  .alias('s')
-  .description('execute the given remote cmd')
-  .option("-f, --file <file>", "give specific file")
-  .action(function(cmd, options) {
-    console.log('exec "%s" using %s mode', cmd, options.file);
-    console.log(arguments);
-  }).on('--help', function() {
-    console.log('  Examples:');
-    console.log();
-    console.log('    $ deploy exec sequential');
-    console.log('    $ deploy exec async');
-    console.log();
-  });
+var glob = require('glob');
 
 program
   .command('*')
   .action(function(env, options) {
-    var modules = _.keys(scanFile(String(fs.readFileSync(env))));
-    if (modules.length) {
-      var keys = modules.map(function (k) { return '+' + k; });
-      // Check whether Sizzle selectors are loaded.
-      if (keys.indexOf('+sizzle') === -1) {
-        if (!(/Selectors?/g).test(keys.join(','))) {
-          keys.unshift('-sizzle');
-          keys.unshift('+selector-native');
+
+    function getModulesFromFile(file) {
+      return _.keys(scanFile(String(fs.readFileSync(env + file))));
+    }
+
+    function processFiles(files) {
+      var modules = [];
+      modules = _.uniq(_.flatten(files.map(getModulesFromFile)));
+      if (modules.length) {
+        var keys = modules.map(function (k) { return '+' + k; });
+        // Check whether Sizzle selectors are loaded.
+        if (keys.indexOf('+sizzle') === -1) {
+          if (!(/Selectors?/g).test(keys.join(','))) {
+            keys.unshift('-sizzle');
+            keys.unshift('+selector-native');
+          }
+          else {
+            keys.unshift('+sizzle');
+          }
         }
-        else {
-          keys.unshift('+sizzle');
+        // Core is always needed.
+        if (keys.indexOf('+core') === -1) {
+          keys.unshift('+core');
         }
+        console.log('grunt build:*' + (keys.length ? ':' + keys.join(':') : '*') + ' && grunt uglify');
       }
-      // Core is always needed.
-      keys.unshift('+core');
-      console.log('grunt build:*' + (keys.length ? ':' + keys.join(':') : '*') + ' && grunt uglify');
+      else {
+        console.log('jQuery is not used.');
+      }
+    }
+
+    if (env.indexOf('.js') === -1) {
+      env = env[env.length - 1] === '/' ? env : env + '/';
+      glob("**/*.js", {cwd: env}, function (er, files) {
+        debug(files);
+        processFiles(files);
+      });
     }
     else {
-      console.log('jQuery is not used.');
+      var files = [env];
+      debug(files);
+      processFiles(files);
     }
+
   });
 
 program.parse(process.argv);
